e7e2606f794ae6183bb434574eec7a4052f71a7a
Diana Tinoco <dtinoco152@gmail.com>
2022-03-02 08:58:41 -0600

    Adding babel-watch

--- server/routes/index.js
+++ server/routes/index.js
@@ -5,7 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   // render manda a renderizar (generar y entregar)
   // la vista a el cliente (index = plantilla y  {title = Datos } )
-  let emojieDataset = ['😎','🗼','😏','☕','🙋‍♂️','👏','🤓','🖤'];
+  let emojieDataset = ['😎','🗼','😏','☕','🙋‍♂️','👏','🤓','🖤','💨','💫','🕳'];
   let emojie = emojieDataset[Math.floor(Math.random()* emojieDataset.length)];
   res.render('index', 
   // Este el el View_Model
