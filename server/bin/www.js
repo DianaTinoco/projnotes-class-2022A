0235ec1de986ff80618de359e676631b661a8bb3
Diana Tinoco <dtinoco152@gmail.com>
2022-03-07 08:54:12 -0600

    Modernización del Generador de Express

--- server/bin/www.js
+++ server/bin/www.js
@@ -6,15 +6,19 @@
 
 // var app = require('../app');
 import app from '../app';
-var debug = require('debug')('projnotes:server');
-var http = require('http');
-const { colors } = require('debug/src/browser');
+//var debug = require('debug')('projnotes:server');
+import Debug from 'debug';
+import http from 'http';
+
+
+// Creando instancia del debugger
+const debug  = Debug("projnotes:server");
 
 /**
  * Get port from environment and store in Express.
  */
 /**process.env.PORT Forma de acceder a la variable de entorno */
-var port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(process.env.PORT || '3000');
 // app es una instancia de ExpressJs[ ] [ NODE ]
 app.set('port', port);
 
@@ -22,7 +26,7 @@ app.set('port', port);
  * Create HTTP server.
  */
 
-var server = http.createServer(app); // (req, res, next, err) => {}
+const server = http.createServer(app); // (req, res, next, err) => {}
 
 /**
  * Listen on provided port, on all network interfaces.
@@ -38,7 +42,7 @@ server.on('listening', onListening);
  */
 
 function normalizePort(val) {
-  var port = parseInt(val, 10);
+  const port = parseInt(val, 10);
 
   if (isNaN(port)) {
     // named pipe
@@ -69,11 +73,11 @@ function onError(error) {
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(bind + ' requires elevated privileges');
+      console.error(`${bind} requires elevated privileges`);
       process.exit(1);
       break;
     case 'EADDRINUSE':
-      console.error(bind + ' is already in use');
+      console.error(`${bind}  is already in use`);
       process.exit(1);
       break;
     default:
@@ -86,11 +90,11 @@ function onError(error) {
  */
 
 function onListening() {
-  var addr = server.address();
-  var bind = typeof addr === 'string'
-    ? 'pipe ' + addr
-    : 'port ' + addr.port;
-  debug('Listening on ' + bind);
-  console.log(`Servidor escuchando... en ${app.get('port')}`)
+  const addr = server.address();
+  const bind = typeof addr === "string"
+    ? `pipe ${addr}`
+    : `port ${addr.port}`;
+  debug(`Listening on  ${bind}`);
+  console.log(`✍Servidor escuchando... en 👂 ${app.get('port')}`)
 
 }
