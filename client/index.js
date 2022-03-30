2e56ab8830a6313bf8dad14b6a8d21470c8618ae
Diana Tinoco <dtinoco152@gmail.com>
2022-03-30 07:56:33 -0600

    Testing de ES06 en Front-end

--- client/index.js
+++ client/index.js
@@ -1,1 +1,24 @@
 console.log("🎁Webpack Working!!!");
+
+// Default parameters
+let show = (m = "😝") => {
+    console.log(m)
+};
+show();
+
+// Promises
+function resolveAfter2Seconds(){
+    return new Promise((resolve) => {
+        setTimeout(( ) => {
+            resolve("function resolve");
+        }, 10000);
+    });
+}
+
+async function asyncCall(){
+    console.log("Calling asyn function!!!");
+    const result = await resolveAfter2Seconds();
+    console.log(result); // Imprime "function resolve" en la consola
+}
+
+asyncCall();
