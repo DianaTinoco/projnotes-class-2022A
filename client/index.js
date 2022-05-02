2ad3c3e41afca31d37d3050e48733f5b6df82c96
Diana Tinoco <dtinoco152@gmail.com>
2022-05-02 07:50:33 -0500

    S28 ABnB Configurado ok

--- client/index.js
+++ client/index.js
@@ -1,28 +1,30 @@
-// Incorporando estilos a mi bundle
-import './styles/style.css'
-import './styles/mystyle.css'
+/* eslint-disable no-console */
 
-console.log("🎁Front-End Working!!!");
+// Incorporando estilos a mi bundle
+import './styles/style.css';
+import './styles/mystyle.css';
+
+console.log('🎁Front-End Working!!!');
 
 // Default parameters
-let show = (m = "😝") => {
-    console.log(m)
+const show = (m = '😝') => {
+  console.log(m);
 };
 show();
 
 // Promises
-function resolveAfter2Seconds(){
-    return new Promise((resolve) => {
-        setTimeout(( ) => {
-            resolve("function resolve");
-        }, 10000);
-    });
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('function resolve');
+    }, 10000);
+  });
 }
 
-async function asyncCall(){
-    console.log("Calling async function!!!");
-    const result = await resolveAfter2Seconds();
-    console.log(result); // Imprime "function resolve" en la consola
+async function asyncCall() {
+  console.log('Calling async function!!!');
+  const result = await resolveAfter2Seconds();
+  console.log(result); // Imprime "function resolve" en la consola
 }
 
 asyncCall();
