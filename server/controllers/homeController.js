242c0180345a28b056c9612957003cccdb947ced
Diana Tinoco <dtinoco152@gmail.com>
2022-05-16 09:57:05 -0500

    S38 About increment

--- server/controllers/homeController.js
+++ server/controllers/homeController.js
@@ -30,7 +30,10 @@ const about = (req, res) => {
   res.render('home/aboutView', {
     name: 'Diana Laura',
     email: 'dtinoco152@gmail.com',
-    url: 'https:',
+    url: 'https://github.com/DianaTinoco/primer-pwpcI-2021',
+    description:
+      'Aplicación que te permite registrar ideas de proyectos. PwpcII - 2022A',
+    version: '0.0.alpha',
   });
 };
 
