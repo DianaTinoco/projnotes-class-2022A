9fdfaf6c77807d2206555b68273f569422877caa
Diana Tinoco <dtinoco152@gmail.com>
2022-05-18 08:28:35 -0500

    S38 Barra de navegacion movil

--- client/index.js
+++ client/index.js
@@ -1,29 +1,14 @@
 /* eslint-disable no-console */
+/* global M */
 
 // Incorporando estilos a mi bundle
 import './styles/mystyle.css';
 
-console.log('🎁Front-End Working!!!');
-
-// Default parameters
-const show = (m = '😝') => {
-  console.log(m);
-};
-show();
-
-// Promises
-function resolveAfter2Seconds() {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve('function resolve');
-    }, 10000);
-  });
-}
-
-async function asyncCall() {
-  console.log('Calling async function!!!');
-  const result = await resolveAfter2Seconds();
-  console.log(result); // Imprime "function resolve" en la consola
-}
-
-asyncCall();
+/* Inicializando elementos de materializecss */
+document.addEventListener('DOMContentLoaded', () => {
+  // Obteniendo la referencia a la barra de navegacion
+  // lateral
+  const sideNavs = document.querySelectorAll('.sidenav');
+  // eslint-disable-next-line no-undef
+  M.Sidenav.init(sideNavs);
+});
